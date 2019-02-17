0007f83f85fbb033395cc1b4ef644599ea08058c
vdanilau <47238767+vdanilau@users.noreply.github.com>
2019-02-17 18:33:57 +0300

    Create Task_1.ps1

---
+++ Task_1.ps1
@@ -0,0 +1,6 @@
+﻿#1.	При помощи WMI перезагрузить все виртуальные машины.
+
+$cred = get-credential administrator #учетная запись администратора (одинаковая на всех VM)
+$VM=@("VM1","VM2","VM3") #перечень VM
+    #Set-Item WSMan:\localhost\Client\TrustedHosts -Value $VM # добавили в хорошеизвестные на Хосте
+Get-WmiObject win32_operatingsystem -ComputerName $VM -Credential $cred | Invoke-WMIMethod -name Reboot #перезагрузили
