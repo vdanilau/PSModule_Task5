2735854691f6c5c8aa73d4227b8dce24527094b6
vdanilau <47238767+vdanilau@users.noreply.github.com>
2019-02-17 18:34:02 +0300

    Create Task_3.ps1

---
+++ Task_3.ps1
@@ -0,0 +1,7 @@
+﻿#3.	Настроить PowerShell Remoting, для управления всеми виртуальными машинами с хостовой.
+
+#$VM=@("VM1","VM2","VM3")
+$cred = get-credential administrator #учетная запись администратора (одинаковая на всех VM)
+Enter-PSSession -ComputerName VM2 -Credential $cred  #имя нужной машины вводим руками!
+
+#Exit-PSSession
