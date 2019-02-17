7cc45f30e461dfc5cd634ac488f3ffc589f9cea6
vdanilau <47238767+vdanilau@users.noreply.github.com>
2019-02-17 18:34:00 +0300

    Create Task_2.ps1

---
+++ Task_2.ps1
@@ -0,0 +1,9 @@
+﻿#2.	При помощи WMI просмотреть список запущенных служб на удаленном компьютере. 
+
+#Set-Item WSMan:\localhost\Client\TrustedHosts -Value $VM # добавили в хорошеизвестные на Хосте
+$cred = get-credential administrator #учетная запись администратора (одинаковая на всех VM)
+#$VM=@("VM1","VM2","VM3") #перечень VM
+gwmi win32_service  -ComputerName VM1 -Credential $cred | where state -eq running |ft #берем запущенные службы
+
+
+
