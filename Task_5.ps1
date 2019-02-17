282ccab3c9aa407969b069f6c69b1cf019fd4253
vdanilau <47238767+vdanilau@users.noreply.github.com>
2019-02-17 18:34:08 +0300

    Create Task_5.ps1

---
+++ Task_5.ps1
@@ -0,0 +1,27 @@
+﻿#5.	Создать конфигурацию сессии с целью ограничения использования всех команд, кроме просмотра содержимого дисков.
+
+######### Это прописываем на удаленном хосте ##############
+Param (
+$path="C:"
+       )
+if (Test-Path $path)
+{
+New-PSSessionConfigurationFile -Path $path\Danilau.pssc -VisibleCmdlets get-childitem, dir, ls, Get-Help, Exit-PSSession, Get-Command, Get-FormatData, Measure-Object, Out-Default, Select-Object
+Test-PSSessionConfigurationFile $path\Danilau.pssc
+}
+else
+{
+Write-Output "Directory doesn't exist"
+}
+$cred = get-credential administrator
+Register-PSSessionConfiguration -Name DanilauSes -Path $path\Danilau.pssc -RunAsCredential $cred -ShowSecurityDescriptorUI -Force
+
+#Get-PSSessionConfiguration -Name * 
+#Unregister-PSSessionConfiguration -Name DanilauSes
+
+######################        #######################################
+
+###################### Теперь команду вводим на хосте с которого ходим устанавливать сессию ###############
+
+Enter-PSSession -ComputerName VM2 -Credential $cred -ConfigurationName DanilauSes 
+#Exit-PSSession
