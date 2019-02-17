0551bb70eb4e3c66ec693031e93e3f3e3003d288
vdanilau <47238767+vdanilau@users.noreply.github.com>
2019-02-17 18:34:05 +0300

    Create Task_4.ps1

---
+++ Task_4.ps1
@@ -0,0 +1,5 @@
+﻿#4.	Для одной из виртуальных машин установить для прослушивания порт 42658. 
+#Проверить работоспособность PS Remoting.
+
+Set-Item WSMan:\localhost\listener\listener*\port -Value 42658 #команду прописываем на удаленной машине (я прописал на VM1)
+Invoke-Command -ScriptBlock {Get-Service} -ComputerName VM1 -Credential administrator -Port 42658 #для проверки 
